efcd03a5e3d87d026e026b943364490fd45f373f
taliesincochran <tdcochran2@gmail.com>
2018-06-13 10:30:17 -0400

    added conditionally rendered buttons to replicate menu for less advanced user's navigation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,36 @@
-import React from 'react'
+import React from 'react';
+import Link from 'gatsby-link';
+import PropTypes from 'prop-types';
 
 const Footer = (props) => (
     <footer id="footer">
         <div className="inner">
             <ul className="icons">
-                <li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
+                {/*<li><a href="#" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>*/}
                 <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
                 <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
-                <li><a href="#" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
-                <li><a href="#" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
+                {props.pathname=="/contact"?"":(
+                    <li><Link to="/contact" className="button">Contact</Link></li>
+                )}
+                {props.pathname=="/"?"":(
+                    <li><Link to="/" className="button">Home</Link></li>
+                )}
+    
+                {/*
+                //To Do
+                
+                <li><Link to="/availability" className="button">Availability</Link></li>
+                <li><Link to="/login" className="button">Parent Login</Link></li>
+                */}
+
             </ul>
             <ul className="copyright">
-                <li>&copy; Untitled</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li>
+                <li>&copy; Taliesin Cochran 2018</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li>
             </ul>
         </div>
     </footer>
 )
-
+Footer.propTypes = {
+    pathname: PropTypes.string
+}
 export default Footer

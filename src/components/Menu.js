558e60899f2d446674aadc08423b6219db8e96f4
taliesincochran <tdcochran2@gmail.com>
2018-06-13 10:15:55 -0400

    modified menu to only display initial pages

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,13 +7,17 @@ const Menu = (props) => (
         <div className="inner">
             <ul className="links">
                 <li><Link onClick={props.onToggleMenu} to="/">Home</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/landing">Landing</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/contact">Contact</Link></li>
+                {/*
                 <li><Link onClick={props.onToggleMenu} to="/generic">Generic</Link></li>
                 <li><Link onClick={props.onToggleMenu} to="/elements">Elements</Link></li>
+                */}
             </ul>
             <ul className="actions vertical">
+                {/*
                 <li><a href="#" className="button special fit">Get Started</a></li>
                 <li><a href="#" className="button fit">Log In</a></li>
+                */}
             </ul>
         </div>
         <a className="close" onClick={props.onToggleMenu} href="javascript:;">Close</a>

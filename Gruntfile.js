7836fc3170dfaf49af27942b2437c3dd9e91fda0
taliesincochran <tdcochran2@gmail.com>
2018-07-13 19:48:02 -0400

    got rid of useless files

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,16 +12,10 @@ module.exports = function(grunt) {
         dest: 'build/<%= pkg.name %>.min.js'
       }
     },
-    less: {
-      options: {
-        ieCompat: true
-      }
-    }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-less');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify']);

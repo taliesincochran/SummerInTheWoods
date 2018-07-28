7c1778874465dbb76ad77d95c7c0e8309496cdc8
kermitage <katherine.sandiego@gmail.com>
2018-07-28 15:32:39 -0400

    removed instagram

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,11 +11,10 @@ const Footer = (props) => {
         <div className="inner">
             <ul className="icons">
                 <ul>
-                    <li><a href="https://www.facebook.com/freeplayisparamount/" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
-                    <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
+                    <li><a href="https://www.facebook.com/freeplayisparamount/" target="_blank" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
+                    {/*<li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>*/}
                 </ul>
                 <Navigation button={true} pathname={props.pathname} state={props.state} />
-
             </ul>
             <ul className="copyright">
                 <li>&copy; Trakai Web Development</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li>

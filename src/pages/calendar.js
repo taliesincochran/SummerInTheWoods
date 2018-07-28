550b543bdc4569b0007e1cdbf0f5c48a7b042c64
kermitage <katherine.sandiego@gmail.com>
2018-07-28 14:32:06 -0400

    fix for fast loading

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -19,9 +19,11 @@ class Calendar extends React.Component {
         }
     }
     componentDidMount () {
-        if(this.props.location.state) {                                       
-            let {year, yearsArray, chosenYear, rawCampTimes} = this.props.location.state;
-            this.setState({year: year, yearsArray: yearsArray, chosenYear:chosenYear, rawCampTimes: rawCampTimes, views: this.getViews(this.props.location.state.chosenYear), views2: this.getViews(parseInt(this.props.location.state.chosenYear) + 1)}, () => console.log("CALENDAR STATE", this.state))      
+        if(this.props.location.state) {      
+            if(this.props.location.state.campTimes) {                                 
+                let {year, yearsArray, chosenYear, rawCampTimes} = this.props.location.state;
+                this.setState({year: year, yearsArray: yearsArray, chosenYear:chosenYear, rawCampTimes: rawCampTimes, views: this.getViews(this.props.location.state.chosenYear), views2: this.getViews(parseInt(this.props.location.state.chosenYear) + 1)}, () => console.log("CALENDAR STATE", this.state))      
+            }
         }    
     }
     handleYearSelect = year => {
@@ -63,51 +65,55 @@ class Calendar extends React.Component {
     }
     render() {
         return(
-            !this.props.location.state?<Redirect to="/"/>:
-            <div>
-                <Helmet>
-                    <title>Summer In The Woods</title>
-                    <meta name="description" content="Calendar Page" />
-                </Helmet>
-                <div id="main">
-                    {this.props.location.state.yearsArray.length > 1?
-                        <div>
-                            <Checkbox style={{display: "inline-block"}} name="year1" value={this.props.location.state.yearsArray[0]} onChange={this.handleYearSelect} checked={this.state.chosenYear == this.props.location.state.yearsArray[0]} className='float-left' value={this.props.location.state.yearsArray[0]} onClick={() => this.handleYearSelect(this.props.location.state.yearsArray[0])} text={this.props.location.state.yearsArray[0]} />
-                            <Checkbox style={{display: "inline-block"}} name="year2" value={this.props.location.state.yearsArray[1]} onChange={this.handleYearSelect} checked={this.state.chosenYear == this.props.location.state.yearsArray[1]} className='float-left' value={this.props.location.state.yearsArray[0]} onClick={() => this.handleYearSelect(this.props.location.state.yearsArray[1])} text={this.props.location.state.yearsArray[1]} />
-                        </div>
-                    :
-                        <div>
-                            <Checkbox style={{display: "inline-block"}} name="year1" value={this.props.location.state.yearsArray[0]} onChange={this.handleYearSelect} checked={true} className='float-left' value={this.props.location.state.yearsArray[0]} onClick={() => this.handleYearSelect(this.props.location.state.yearsArray[0])} text={this.props.location.state.yearsArray[0]} />
-                        </div>
-                    }
+            !this.props.location.state?
+                <Redirect to="/"/>:
+                !this.props.location.state.campTimes?
+                    <Redirect to="/"/>
+                :
                     <div>
-                        <Checkbox name="June" value={5} onChange={()=> this.handleMonthSelect(5)} checked={this.state.month == 5} style={{display: "inline-block"}} value={5} onClick={() => this.handleMonthSelect(5)} text="June"/>
-                        <Checkbox name="July" value={6} onChange={()=> this.handleMonthSelect(6)} checked={this.state.month == 6} style={{display: "inline-block"}} value={6} onClick={() => this.handleMonthSelect(6)} text="July"/>
-                        <Checkbox name="August" value={7} onChange={()=> this.handleMonthSelect(7)} checked={this.state.month == 7} style={{display: "inline-block"}} value={7} onClick={() => this.handleMonthSelect(7)} text="August"/>
-                    </div>
-                    <div className="inner">
-                        {this.state.views?
-                            this.state.chosenYear==this.props.location.state.yearsArray[0]?
-                                this.state.views.map((view, i)=>
-                                    this.state.month == i + 5?
-                                        <Selectable {...this.props} key={i} campTimes={this.props.location.state.campTimes[0]} year={this.props.location.state.yearsArray[0]} index={0} title={view.month} defaultDate={view.date} />                        
+                        <Helmet>
+                            <title>Summer In The Woods</title>
+                            <meta name="description" content="Calendar Page" />
+                        </Helmet>
+                        <div id="main">
+                            {this.props.location.state.yearsArray.length > 1?
+                                <div>
+                                    <Checkbox style={{display: "inline-block"}} name="year1" value={this.props.location.state.yearsArray[0]} onChange={this.handleYearSelect} checked={this.state.chosenYear == this.props.location.state.yearsArray[0]} className='float-left' value={this.props.location.state.yearsArray[0]} onClick={() => this.handleYearSelect(this.props.location.state.yearsArray[0])} text={this.props.location.state.yearsArray[0]} />
+                                    <Checkbox style={{display: "inline-block"}} name="year2" value={this.props.location.state.yearsArray[1]} onChange={this.handleYearSelect} checked={this.state.chosenYear == this.props.location.state.yearsArray[1]} className='float-left' value={this.props.location.state.yearsArray[0]} onClick={() => this.handleYearSelect(this.props.location.state.yearsArray[1])} text={this.props.location.state.yearsArray[1]} />
+                                </div>
+                            :
+                                <div>
+                                    <Checkbox style={{display: "inline-block"}} name="year1" value={this.props.location.state.yearsArray[0]} onChange={this.handleYearSelect} checked={true} className='float-left' value={this.props.location.state.yearsArray[0]} onClick={() => this.handleYearSelect(this.props.location.state.yearsArray[0])} text={this.props.location.state.yearsArray[0]} />
+                                </div>
+                            }
+                            <div>
+                                <Checkbox name="June" value={5} onChange={()=> this.handleMonthSelect(5)} checked={this.state.month == 5} style={{display: "inline-block"}} value={5} onClick={() => this.handleMonthSelect(5)} text="June"/>
+                                <Checkbox name="July" value={6} onChange={()=> this.handleMonthSelect(6)} checked={this.state.month == 6} style={{display: "inline-block"}} value={6} onClick={() => this.handleMonthSelect(6)} text="July"/>
+                                <Checkbox name="August" value={7} onChange={()=> this.handleMonthSelect(7)} checked={this.state.month == 7} style={{display: "inline-block"}} value={7} onClick={() => this.handleMonthSelect(7)} text="August"/>
+                            </div>
+                            <div className="inner">
+                                {this.state.views?
+                                    this.state.chosenYear==this.props.location.state.yearsArray[0]?
+                                        this.state.views.map((view, i)=>
+                                            this.state.month == i + 5?
+                                                <Selectable {...this.props} key={i} campTimes={this.props.location.state.campTimes[0]} year={this.props.location.state.yearsArray[0]} index={0} title={view.month} defaultDate={view.date} />                        
+                                            :null
+                                    ):null
+                                :null
+                                }
+                                {this.state.views2?
+                                    this.state.chosenYear==this.props.location.state.yearsArray[1]?
+                                        this.state.views2.map((view, i)=>
+                                            this.state.month == i + 5?
+                                                <Selectable {...this.props} key={i} year={this.props.location.state.yearsArray[1]} campTimes={this.props.location.state.campTimes[1]} index={1} title={view.month} defaultDate={view.date} />  
+                                            :null
+                                        )
                                     :null
-                            ):null
-                        :null
-                        }
-                        {this.state.views2?
-                            this.state.chosenYear==this.props.location.state.yearsArray[1]?
-                                this.state.views2.map((view, i)=>
-                                    this.state.month == i + 5?
-                                        <Selectable {...this.props} key={i} year={this.props.location.state.yearsArray[1]} campTimes={this.props.location.state.campTimes[1]} index={1} title={view.month} defaultDate={view.date} />  
-                                    :null
-                                )
-                            :null
-                        :null
-                        }
-                    </div>
-                </div>
-            </div>
+                                :null
+                                }
+                            </div>
+                        </div>
+                    </div>      
         )        
     }
 }

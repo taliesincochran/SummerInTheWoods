958305b4ef1995c60a694ef447f55016f130d37e
OtherSuccesses <viskaslietuvai@gmail.com>
2019-01-12 18:39:50 -0500

    mail integration and only cleanup needed for next iteration

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -68,6 +68,7 @@ class TemplateWrapper extends Component {
             let yearsArray = rawYearsArray.slice(yearIndex)
             //Make an array of relavent camptimes
             let campTimes = yearsArray.map(year => rawCampTimes[year])
+            console.log(';;;;;;;;;;;;;;;;;',campTimes);
             let data = {
                 campTimes,
                 rawCampTimes,
@@ -113,7 +114,7 @@ class TemplateWrapper extends Component {
         this.setState({ yearChosen: event.target.value });
     }
     calculateCost() {
-        let weeksArray = [this.state.week1, this.state.week2, this.state.week3, this.state.week4, this.state.week5, this.state.week6, this.state.week7, this.state.week8];
+        let weeksArray = [this.state.week0,this.state.week1, this.state.week2, this.state.week3, this.state.week4, this.state.week5, this.state.week6, this.state.week7, this.state.week8,this.week9];
         let threeDayArray = weeksArray.filter(value=> value == 3);
         let fiveDayArray = weeksArray.filter(value=> value == 5);
         let threeDayCount = threeDayArray.length() + 1;
--- src/pages/mail.js
+++ src/pages/mail.js
@@ -5,26 +5,97 @@ import { Redirect } from "react-router-dom"
 
 const Mail = (props) => {
     let message = '';
+    let state = props.state;
+    let { 
+            date,
+            amountDue,
+            savings,
+            localTimezoneOffset,
+            Week0,
+            Week1,
+            Week2,
+            Week3,
+            Week4,
+            Week5,
+            Week6,
+            Week7,
+            Week8,
+            Week9,
+            weekArray,
+            childFirstName,
+            childLastName,
+            age,
+            birthday,
+            allergies,
+            parent1Name,
+            parent1Phone,
+            parent1Email,
+            parent1EmailVerify,
+            parent2Name,
+            parent2Phone,
+            parent2Email,
+            emergency1Name,
+            emergency1Relationship,
+            emergency1Phone,
+            emergency2Name,
+            emergency2Relationship,
+            emergency2Phone,
+            physicianName,
+            physicianPhone,
+            dentistName,
+            dentistPhone,
+            address
+        } = state;
+
+
     const gotchaStyle = {
         display: 'none'
     }
     if(props.location.state) {
-        console.log(props.location.state.parent1Name)
-        message = props.location.state.parent1Name + "has filled out an application for your camp. Please email them at " + props.location.state.parent1Email + " the address so that they can mail thier check for the $" + props.location.state.amountDue + " to confirm the hold.";
-    }
-
+        message = parent1Name + "has filled out an application for your camp. Please email them at " + parent1Email + ". Send them the address so that they can mail thier check for the $" + amountDue + " and a paypal bill to confirm the hold.\n" +
+            "amountDue:" + amountDue + "\n" + 
+            "Week 1:" + Week0 + " days\n" + 
+            "Week 2:" + Week1 + " days\n" + 
+            "Week 3:" + Week2 + " days\n" + 
+            "Week 4:" + Week3 + " days\n" + 
+            "Week 5:" + Week4 + " days\n" + 
+            "Week 6:" + Week5 + " days\n" + 
+            "Week 7:" + Week6 + " days\n" + 
+            "Week 8:" + Week7 + " days\n" + 
+            "Week 9:" + Week8 + " days\n" + 
+            "Week 10:" + Week9 + " days\n" + 
+            "child's first name:" + childFirstName + "\n" + 
+            "child's last name:" +  childLastName + "\n" + 
+            "age:" + age + "\n" + 
+            "birthday:" + birthday + "\n" + 
+            "allergies:" + allergies + "\n" + 
+            "parent name:" + parent1Name + "\n" + 
+            "parent phone:" + parent1Phone + "\n" + 
+            "parent email:" + parent1Email + "\n" + 
+            "second parent name:" +  parent2Name + "\n" + 
+            "second parent phone:" + parent2Phone + "\n" + 
+            "second parent email:" + parent2Email + "\n" + 
+            "emergency contact name:" + emergency1Name + "\n" + 
+            "first emergency contact relationship:" + emergency1Relationship + "\n" + 
+            "first emergency contact phone:" +    emergency1Phone + "\n" + 
+            "second emergency contact name:" + emergency2Name + "\n" + 
+            "second emergency contact relationship:" + emergency2Relationship + "\n" + 
+            "second emergency contact phone:" +    emergency2Phone + "\n" + 
+            "physician name:" +  physicianName + "\n" + 
+            "physician phone:" + physicianPhone + "\n" + 
+            "dentist name:" +    dentistName + "\n" + 
+            "dentist phone:" +   dentistPhone + "\n" + 
+            "address:" + address + "\n"
+            "timezone offset. This should equal 300 or 400:" + localTimezoneOffset + "\n" 
+        }
+        console.log(message)
         return(
         !props.location.state?<Redirect to="/"/>:
         <div>
-            <Helmet>
-                <title>Summer In The Woods</title>
-                <meta name="description" content="Mail Page" />
-            </Helmet>
-            <BannerLanding bannerClass="contactBanner" />
+            <p>Add any additional information below.  Once the application is submitted, we will email you a bill.</p>
             <div id="main">
                 <div className="inner">
                     <section>
-                        <p>Please fill out this form and we will email you the address to send the check for the ${props.location.state.amountDue}.00</p>
                         <form method="post" action="https://formspree.io/taliesincochran@gmail.com">
                             <div className="field half first">
                                 <label htmlFor="name">Name</label>
@@ -39,11 +110,11 @@ const Mail = (props) => {
                                 <input type="hidden" name="_next" value="/" />
                             </div>
                             <div className="field" style={{display:"none"}}>
-                                <label htmlFor="message">Message</label>
+                                <label htmlFor="message">Additional information</label>
                                 <textarea name="message" id="message" rows="6">{message}</textarea>
                             </div>
                             <ul className="actions">
-                                <li><input type="submit" value="Send Message" className="special" /></li>
+                                <li><input type="submit" value="Send" className="special" /></li>
                                 <li><input type="reset" value="Clear" /></li>
                             </ul>
                         </form>
@@ -55,13 +126,6 @@ const Mail = (props) => {
                                 <h3>Email</h3>
                             {/*The long string of ASCII is to confuse scraping bots*/}
                                 <a className="email" href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#115;&#117;&#109;&#109;&#101;&#114;&#105;&#110;&#116;&#104;&#101;&#119;&#111;&#111;&#100;&#115;&#99;&#97;&#109;&#112;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;">&#115;&#117;&#109;&#109;&#101;&#114;&#105;&#110;&#116;&#104;&#101;&#119;&#111;&#111;&#100;&#115;&#99;&#97;&#109;&#112;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;</a>
-                            </div>
-                        </section>
-                        <section>
-                            <div className="contact-method">
-                                <span className="icon alt fa-phone"></span>
-                                <h3>Phone</h3>
-                                <a href="tel:0000000000">(000) 000-0000</a>
                             </div>
                         </section>
                         <section>

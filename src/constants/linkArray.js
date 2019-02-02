afffe0722feba3cb23ebe83cd57b491af2d478f9
Taliesin Cochran <taliesincochran@gmail.com>
2019-02-02 09:52:38 -0500

    checking out v2 for code

--- src/constants/linkArray.js
+++ src/constants/linkArray.js
@@ -3,29 +3,43 @@ import * as routes from './routes'
 
 const linkArray = [
   {
-    path: routes.HOME, 
+    path: routes.HOME,
     text: "Home",
-    admin: true,
     auth: true,
     nonAuth: true
   },
   {
+    path: routes.ADMIN_CALENDAR_EDITOR,
+    text: "Calendar Editor",
+    auth: true
+  },
+  {
+    path: routes.ADMIN_APPLICATION_VIEW,
+    text: "View Applications",
+    auth: true
+  },
+  {
     path: routes.APPLY,
     text: 'Apply',
-    nonAuth:true
+    nonAuth: true,
+    auth: true
   },
   {
     path: routes.CONTACT,
     text: 'Contact Us',
     auth: true,
-    nonAuth:true
+    nonAuth: true
   },
   {
     path: routes.CALENDAR,
     text: 'Calendar',
-    admin: true,
     auth: true,
     nonAuth: true
   },
+  {
+    path: routes.SIGN_IN,
+    text: 'Sign In',
+    nonAuth: true
+  }
 ]
 export default linkArray

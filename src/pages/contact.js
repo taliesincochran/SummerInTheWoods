dfeab997d76ba374b3630c65d70223e9493e2954
Taliesin Cochran <taliesincochran@gmail.com>
2019-02-02 21:11:59 -0500

    fixed handle change and redirect in contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import BannerLanding from '../components/BannerLanding/'
-import { Redirect } from "react-router-dom"
 
 class Contact extends React.Component {
     constructor(props) {
@@ -9,8 +8,16 @@ class Contact extends React.Component {
         this.state = {
             messasge: '',
             email: '',
-            name: ''
+            name: '',
+            location: '/'
         }
+    }
+    componentDidMount () {
+        if(window !== undefined) {
+            let location = window.location;
+            this.setState({location})
+        }
+
     }
     handleChange = e => {
         let { name, value } = e.target;
@@ -18,39 +25,43 @@ class Contact extends React.Component {
     }
     render() {
         return(
-            !this.props.location.state?<Redirect to="/"/>:
-                <div>
-                    <Helmet>
-                        <title>Summer In The Woods</title>
-                        <meta name="description" content="Contact Page" />
-                    </Helmet>
-                    <BannerLanding bannerClass="contactBanner" />
-                    <div id="main">
-                        <div className="inner">
-                            <section>
-                                <form method="post" action={process.env.GATSBY_EMAIL_CONTACT_TO}>
-                                    <div className="field">
-                                        <label htmlFor="name">name</label>
-                                        <input type="text" name="name" id="name" onChange={this.onChange} value={this.state.name}></input>
-                                    </div>
-                                    <div className="field">
-                                        <label htmlFor="email">email</label>
-                                        <input type="email" name="email" id="email" onChange={this.onChange} value={this.state.email}></input>
-                                    </div>
-                                    <div className="field">
-                                        <label htmlFor="message">Message</label>
-                                        <textarea name="message" id="message" rows="6" onChange={this.onChange} value={this.state.message}></textarea>
-                                    </div>
-                                    <ul className="actions">
-                                        <li><input type="submit" value="Send Message" className="special" /></li>
-                                        <li><input type="reset" value="Clear" /></li>
-                                    </ul>
-                                </form>
-                            </section>
-                        </div>
+            <div>
+                <Helmet>
+                    <title>Summer In The Woods</title>
+                    <meta name="description" content="Contact Page" />
+                </Helmet>
+                <BannerLanding bannerClass="contactBanner" />
+                <div id="main">
+                    <div className="inner">
+                        <section>
+                            <form method="post" action={process.env.GATSBY_EMAIL_CONTACT_TO}>
+                                <input
+                                    type="hidden"
+                                    value={this.state.location}
+                                    name="_redirect"
+                                />
+                                <div className="field">
+                                    <label htmlFor="name">name</label>
+                                    <input type="text" name="name" id="name" onChange={this.handleChange} value={this.state.name}></input>
+                                </div>
+                                <div className="field">
+                                    <label htmlFor="email">email</label>
+                                    <input type="email" name="email" id="email" onChange={this.handleChange} value={this.state.email}></input>
+                                </div>
+                                <div className="field">
+                                    <label htmlFor="message">Message</label>
+                                    <textarea name="message" id="message" rows="6" onChange={this.handleChange} value={this.state.message}></textarea>
+                                </div>
+                                <ul className="actions">
+                                    <li><input type="submit" value="Send Message" className="special" /></li>
+                                    <li><input type="reset" value="Clear" /></li>
+                                </ul>
+                            </form>
+                        </section>
                     </div>
                 </div>
-            )
+            </div>
+        )
     }
 }
 
